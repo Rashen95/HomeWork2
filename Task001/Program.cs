cae26d7593f97dcb8d7ef5e780499d71b8ed9413
PrivalovArtem <rashen95@mail.ru>
2022-08-10 21:00:19 +0300

    Решена первая задача

---
+++ Task001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.Write("Введите любое трехзначное положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 99 || number >= 1000)
+{
+    Console.WriteLine("Введено не верное число");
+}
+else
+{
+    Console.WriteLine(number % 100 / 10);
+}
